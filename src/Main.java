d26eb064df8a31d78aab2368715fbbb5947d0839
cph-mh1088 <112551104+cph-mh1088@users.noreply.github.com>
2023-02-20 10:25:32 +0100

    Added getString and getInt method

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,34 @@ public class Main {
 
     public static void main(String[] args) {
 
+       // System.out.println(getString("Hvad hedder du?"));
+
+        System.out.println(getInt("angiv venligt din skostørrelse"));
+        System.out.println(getInt("angiv venligt din højde"));
+
     }
 
     public static String getString(String s) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println(s + " : ");
+        System.out.print(s + ": ");
         return scanner.nextLine();
 
+    }
+
+    public static int getInt(String s){
+
+        int res;
+
+        while (true) {
+            try {
+                res = Integer.parseInt(getString(s));
+                return res;
+            } catch (Exception e){
+                System.out.println("Husk ikke tal ord");
+            }
+        }
     }
 
 
